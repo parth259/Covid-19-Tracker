e65d730a93806090fc1ca9c0133829ddefd1865d
Parth Patel <68620884+parth259@users.noreply.github.com>
2021-01-28 16:29:32 -0400

    added API to call countries and created UI

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MenuItem,FormControl,Select} from "@material-ui/core"
+import InfoBox from './InfoBox';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('Worldwide');
+
+  //https://disease.sh/v3/covid-19/countries
+
+  // USEEFFECT = Runs a piece of code based on given conditiom 
+
+  useEffect (() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const  countries = data.map((country) => (
+          {
+            name: country.country, //United States,United Kingdom
+            value: country.countryInfo.iso2 // UK,USA,FR
+          }));
+        
+          setCountries(countries);
+        });
+    };
+    
+    getCountriesData();
+
+  }, []);
+  
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
+
   return (
-    <div className="App">
-      <h1>Covid-19 Tracker</h1>
+    <div className="app">
+      <div className="app__header">     
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" onChange={onCountryChange} value={country} >
+        <MenuItem value="Worldwide">Worldwide</MenuItem>
+          {
+            countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem> 
+            ))
+          }
+         
+        </Select>  
+      </FormControl>
+      </div>
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus cases" />
+
+        <InfoBox title="Recovered" />
+
+        <InfoBox title="Deaths" />
+
+
+
+
+
+      </div>
     </div>
   );
 }
